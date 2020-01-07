18506889fb290654a3ec763d27b78107bd7dcf78
qttd1 <1289813875@qq.com>
2020-01-07 20:17:32 +0800

    shuoming

--- src/main/java/com/atguigu/test.java
+++ src/main/java/com/atguigu/test.java
@@ -3,6 +3,9 @@ package com.atguigu;
 public class test {
     public static void main(String[] args) {
         System.out.println("dugujiujian");
+        System.out.println("aaaaaa");
+        System.out.println("aaaaaa");
+        System.out.println("aaaaaa");
     }
 
 }
