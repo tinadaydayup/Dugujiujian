da5ed96dbb6185cc9eae3ab3833ec4810c94dfb2
qttd1 <1289813875@qq.com>
2020-01-07 20:20:13 +0800

    shuoming

--- src/main/java/com/atguigu/abc.java
+++ src/main/java/com/atguigu/abc.java
@@ -1,5 +1,8 @@
 package com.atguigu;
 
 public class abc {
+    public static void main(String[] args) {
+        System.out.println("aaaaaa");
+    }
 
 }
--- src/main/java/com/atguigu/test.java
+++ src/main/java/com/atguigu/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("dugujiujian");
         System.out.println("aaaaaa");
-        System.out.println("aaaaaa");
+        System.out.println("aaaabbaa");
         System.out.println("aaaaaa");
     }
 
